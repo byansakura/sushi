27c54656da37c289078aaeab566e7569f816913f
byansakura <byan.aji@tokopedia.com>
2023-04-29 16:34:47 +0700

    init

---
+++ apps/home/electricity.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.api import VAR
+from statsmodels.tools.eval_measures import rmse
+import matplotlib.pyplot as plt
+import os
+
+def get_predicted_kwh():
+    # load the data
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "CRCS.csv"))
+    df = pd.read_csv(file_path)
+    # Convert 'Date' column to datetime format
+    df['Date'] = pd.to_datetime(df['Date'], format="%m-%d-%Y")
+
+
+    # Set 'Date' column as the index
+    df.set_index('Date', inplace=True)
+    df.index.freq = pd.infer_freq(df.index)
+
+    # create a dataframe with only the required columns (KwH, max_temp, min_temp)
+    df = df[['KwH', 'MaxTemperature', 'MinTemperature']]
+
+    # split the data into train and test sets (use the last 60 rows for testing)
+    train = df[:-30]
+    test = df[-30:]
+
+    # fit the VAR model
+    model = VAR(train)
+    results = model.fit()
+
+    # create lagged values for the test data
+    lags = results.k_ar
+    test_data = test.values
+    history = train.values[train.shape[0]-lags:]
+    predictions = []
+
+    # forecast for the test data
+    for t in range(test_data.shape[0]):
+        model_input = history[-lags:]
+        yhat = results.forecast(model_input, steps=1)
+        predictions.append(yhat[0])
+        history = np.vstack([history, test_data[t]])
+
+    # create a dataframe of predictions and actual values
+    pred_df = pd.DataFrame(predictions, index=test.index, columns=df.columns)
+    actual_df = test
+
+    # get the predicted values as a list of dictionaries
+    predicted_kwh = pred_df['KwH'].reset_index().to_dict('records')
+
+    # return the predicted values
+    return predicted_kwh
---
+++ apps/home/routes.py
@@ -0,0 +1,66 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.home import blueprint
+from flask import render_template, request
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+from .electricity import get_predicted_kwh
+from flask import jsonify
+
+"""
+add predicted_kwh from electricity.py to the render_template so it can be used in building-data.html
+"""
+@blueprint.route('/building-data')
+@login_required
+def building_data():
+    predicted_kwh = get_predicted_kwh()
+    return jsonify(predicted_kwh)
+
+
+
+@blueprint.route('/index')
+@login_required
+def index():
+
+    return render_template('home/index.html', segment='index')
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
